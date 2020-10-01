1609883c90fa115945f713ef7354d348ce5a4fa6
wertyasdf <>
2020-10-01 11:59:27 +0600

    moment library added

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 
 app = Flask(__name__, template_folder='../templates')
@@ -21,6 +23,10 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 from app import routes, models, errors
 
