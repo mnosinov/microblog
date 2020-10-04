8da89cd3f9ec64f034555d148371191c3d136fb7
wertyasdf <>
2020-10-04 22:42:41 +0600

    Full-Text search through posts, using elasticsearch has been applied.

--- config.py
+++ config.py
@@ -19,3 +19,4 @@ class Config(object):
     LANGUAGES = ['en', 'es', 'ru', 'fr']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 3
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
